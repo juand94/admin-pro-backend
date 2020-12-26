d9cdc273635cada1a17041ae041e72c043fabdb2
Juan Diego Garcia Gomez <juan@MacBook-Air-ComunityCloud.local>
2020-12-26 09:51:24 -0500

    Fin seccion 10 -Auth

--- index.js
+++ index.js
@@ -13,20 +13,26 @@ const app = express();
 // Configurar CORS
 app.use(cors());
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Base de Datos
 dbconnection();
 //console.log(process.env);
 
 // Rutas
-app.get('/', (req, res) => {
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
-    // res.status(400).json({
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
+//app.get('/', (req, res) => {
 
-});
+// res.status(400).json({
+//    res.json({
+//        ok: true,
+//        msg: 'Hola Mundo'
+//    });
+
+//});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
